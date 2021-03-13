280fcca0f46cd555f1a9dcb9970250106fb07984
Karanveer <karan_4496@hotmail.com>
2021-03-13 11:55:44 -0500

    Implemented encrpytion and decryption

--- Program.cs
+++ Program.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.IO;
 
+using SimpleAES;
+
 namespace CSEncryption {
     class Program {
 
         readonly struct Args {
             public readonly bool isEncryptMode { get; }
             public readonly string key { get; }
-            public readonly string txtFilePath { get; }
+            public readonly string path { get; }
 
-            public Args(bool isEncryptMode, string key, string txtFilePath) {
+            public Args(bool isEncryptMode, string key, string path) {
                 this.isEncryptMode = isEncryptMode;
                 this.key = key;
-                this.txtFilePath = txtFilePath;
+                this.path = Path.GetFullPath(path);
             }
         }
 
@@ -30,40 +32,76 @@ namespace CSEncryption {
             }
 
             string key = argsList[keyIdx + 1];
-            string txtFilePath = argsList[pathIdx + 1];
+            string path = argsList[pathIdx + 1];
 
-            return new Args(isEncryptMode, key, txtFilePath);
+            return new Args(isEncryptMode, key, path);
         }
 
-        static void Main(string[] args) {
-            Args parsedArgs = GetArgs(args);
-            string text = System.IO.File.ReadAllText(parsedArgs.txtFilePath);
+        private static Action encrypt(string plainText, string key, string path) {
+            string cipherText = AES256.Encrypt(plainText, key);
 
-            var fp = Path.GetFullPath(parsedArgs.txtFilePath);
-            var dirPath = Path.GetDirectoryName(fp);
+            var dirPath = Path.GetDirectoryName(path);
+            var encFileName = Path.GetFileNameWithoutExtension(path) + "_encrypted" + Path.GetExtension(path);
+            var encFilePath = Path.Combine(dirPath, encFileName);
 
-            if (parsedArgs.isEncryptMode) {
-                string cipherText = AES.encrypt(parsedArgs.key, text);
-
-                // write to file
-                var encFileName = Path.GetFileNameWithoutExtension(fp) + "_encrypted" + Path.GetExtension(fp);
-                var encFilePath = Path.Combine(dirPath, encFileName);
+            return () => {
                 File.WriteAllText(encFilePath, cipherText);
+                Console.WriteLine("Wrote encrypted file to: {0}", encFilePath);
+            };
+        }
 
-                return;
+        private static Action decrypt(string cipherText, string key, string path) {
+            string plaintext = AES256.Decrypt(cipherText, key);
+
+            var fileName = Path.GetFileNameWithoutExtension(path);
+            var dirPath = Path.GetDirectoryName(path);
+            string decFileName = fileName.Contains("encrypted")
+                ? fileName.Replace("encrypted", "decrypted") + Path.GetExtension(path)
+                : fileName + "_decrypted" + Path.GetExtension(path);
+            var decFilePath = Path.Combine(dirPath, decFileName);
+
+            return () => {
+                File.WriteAllText(decFilePath, plaintext);
+                Console.WriteLine("Wrote decrypted file to: {0}", decFilePath);
+            };
+        }
+
+        static void Main(string[] cliArgs) {
+            Args args = GetArgs(cliArgs);
+            if (args.isEncryptMode) {
+                Console.WriteLine("Encrypt mode active");
+            }
+            else {
+                Console.WriteLine("Decrypt mode active");
+            }
+            
+            string text = System.IO.File.ReadAllText(args.path);
+
+            if (string.IsNullOrEmpty(text)) {
+                Console.WriteLine("Error: File contains no content.");
+                System.Environment.Exit(2);
+            }
+            if (string.IsNullOrEmpty(args.key)) {
+                Console.WriteLine("Error: Key cannot be null");
+                System.Environment.Exit(3);
             }
 
-            string plaintext = AES.decrypt(parsedArgs.key, text);
-
-            // write to file
-            var fileName = Path.GetFileNameWithoutExtension(fp);
-            string decFileName = fileName.Contains("encrypted")
-                ? fileName.Replace("encrypted", "decrypted") + Path.GetExtension(fp)
-                : fileName + "_decrypted" + Path.GetExtension(fp);
-            var decFilePath = Path.Combine(dirPath, decFileName);
-            File.WriteAllText(decFilePath, plaintext);
-
-            return;
+            try {
+                Action writeFile = args.isEncryptMode
+                    ? encrypt(text, args.key, args.path)
+                    : decrypt(text, args.key, args.path);
+                try {
+                    writeFile();
+                } catch {
+                    Console.WriteLine("Exception while writing file");
+                }
+            } catch {
+                if (args.isEncryptMode) {
+                    Console.WriteLine("Exception while encrypting data.");
+                } else {
+                    Console.WriteLine("Exception while decrypting data. Key may be invalid.");
+                }
+            }
         }
     }
 }
